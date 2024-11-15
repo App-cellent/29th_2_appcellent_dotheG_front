e52712a5ac6c5bec546f18d119d18585f5c77dc0
김세희 <lolry0207@sookmyung.ac.kr>
2024-11-15 21:18:22 +0900

    feat: 버튼 disabled 속성 추가

--- src/components/GradientButton.tsx
+++ src/components/GradientButton.tsx
@@ -15,21 +15,29 @@ interface GradientButtonProps {
   height?: number; // 버튼 높이
   width?: number;  // 버튼 너비
   text: string;    // 버튼 안에 들어갈 텍스트
+  isDisabled: boolean;
 }
 
-const GradientButton: React.FC<GradientButtonProps> = ({ height = 56, width = 328, text }) => {
+const GradientButton: React.FC<GradientButtonProps> = ({ height = 56, width = 328, text, isDisabled = false }) => {
+    const buttonTextColor = isDisabled ? '#C9C9C9' : '#ffffff';
+    const buttonBackgroundColor = isDisabled ? ['#D3D3D3', '#D3D3D3'] : ['#9BC9FE', '#69E6A2'];
+
     const handlePress = () => {
         alert("버튼이 클릭되었습니다!");
       };
+
     return (
-    <TouchableOpacity onPress={handlePress}>
+    <TouchableOpacity
+        onPress={handlePress}
+        disabled={isDisabled}
+        style={[styles.button, isDisabled && styles.disabledButton]}>
         <LinearGradient
           start={{x: 0, y: 0}}
           end={{x: 1, y: 0}}
-          colors={['#9BC9FE', '#69E6A2']}
+          colors={buttonBackgroundColor}
           style={[styles.linearGradient, {height, width}]}
           >
-          <Text style={styles.buttonText}>
+          <Text style={[styles.buttonText, { color: buttonTextColor }]}>
             {text}
           </Text>
         </LinearGradient>
@@ -38,6 +46,9 @@ const GradientButton: React.FC<GradientButtonProps> = ({ height = 56, width = 32
 };
 
 const styles = StyleSheet.create({
+  disabledButton: {
+    opacity: 0.5,
+  },
   linearGradient: {
     paddingLeft: 15,
     paddingRight: 15,
@@ -49,7 +60,6 @@ const styles = StyleSheet.create({
   buttonText: {
     fontSize: 16,
     fontWeight: 700,
-    color: '#ffffff',
     backgroundColor: 'transparent',
   },
 });
