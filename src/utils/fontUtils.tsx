6c7e9fd1b3603c07b7daf4d5e41b5bebe34a94da
김세희 <lolry0207@sookmyung.ac.kr>
2024-11-26 21:28:53 +0900

    feat: 반응형 폰트 관련 fontUtils.tsx 파일 구현

---
+++ src/utils/fontUtils.tsx
@@ -0,0 +1,8 @@
+// utils/fontUtils.js
+
+import { PixelRatio } from 'react-native';
+
+export const getFontSize = (size) => {
+  const fontScale = PixelRatio.getFontScale();  // 화면의 폰트 크기 비율을 구함
+  return size / fontScale;  // 화면 크기에 맞춰 폰트 크기 반환
+};
