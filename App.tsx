42b1ba6c5e979396fd234f6a0e3733ccc0a24681
Yeonjae Lee <lyeonj@sookmyung.ac.kr>
2025-01-31 21:56:43 +0900

    splash

--- App.tsx
+++ App.tsx
@@ -6,12 +6,21 @@
  */
 
 import React, { useState, createContext } from 'react';
+import { useEffect } from 'react';
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import StackNavigator from './src/navigator/StackNavigator';
 import PushNotification from "react-native-push-notification";
 import {enableScreens} from 'react-native-screens';
+import SplashScreen from "react-native-splash-screen";
 
 function App(): React.JSX.Element {
+
+  useEffect(() => {
+    setTimeout(() => {
+      SplashScreen.hide();
+    }, 2000);
+  });
+
     enableScreens();
   const customTheme = {
     ...DefaultTheme,
